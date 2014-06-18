3e07b0d087c0ddf7234de57fa5a7af2d18c9712d
Kelsey <kma3a@virginia.edu>
2014-06-18 16:34:29 -0500

    made it so my test is in context

--- spec/models/board_spec.rb
+++ spec/models/board_spec.rb
@@ -1,7 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Board, :type => :model do
-  it 'should take parameters' do
-  	expect{Board.new}.to raise_error(ArgumentError)
-  end
+  context '#initialize' do
+	  it 'should take parameters' do
+	  	expect{Board.new}.to raise_error(ArgumentError)
+	  end
+	end
+
 end
