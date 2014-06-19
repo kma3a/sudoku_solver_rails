016929376fe60b03fcd5904e0af8fd365783e503
Kelsey <kma3a@virginia.edu>
2014-06-19 16:05:44 -0500

    added initialize to the cell class

--- app/models/cell.rb
+++ app/models/cell.rb
@@ -1,2 +1,9 @@
-class Cell < ActiveRecord::Base
+class Cell
+
+	attr_accessor :value
+
+	def initialize(args)
+		@value = args[:value]
+	end
+
 end
--- spec/models/cell_spec.rb
+++ spec/models/cell_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Cell, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:cell) {Cell.new(value: 1)}
+  let(:cell2) {Cell.new(value: [1,2,3,4,5,6,7,8,9])}
+
+  context '#initialize' do
+  	it 'should be an instance of cell' do
+  		expect(cell).to be_an_instance_of(Cell)
+  	end
+  end
 end
